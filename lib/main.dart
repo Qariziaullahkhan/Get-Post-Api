d9bc881bfb9bc6805c72ffe6e6a94d12894684fc
Malik Zia <malikziakhan0315@gmail.com>
2024-02-05 10:28:37 +0500

    we can creat Get and Post API

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:session13_api/screens/post_list_screen.dart';
+import 'package:session13_api/screens/single_post_get_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PostListScreen(),
+    );
+  }
+}
